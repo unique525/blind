705687449e35cabed6eb00dc60fef6e7f52cc752
525 <76349186@qq.com>
2015-09-09 15:07:20 +0800

    525f

--- Assets/script/input/test.cs
+++ Assets/script/input/test.cs
@@ -3,14 +3,11 @@ using System.Collections;
 
 public class test : MonoBehaviour
 {
-
+	Animator animator;
 	// Use this for initialization
 	void Start ()
 	{
-		GameObject button = Instantiate (Resources.Load ("button")) as  GameObject;
-		//RectTransform rect = button.GetComponent<RectTransform> ();
-		//rect.SetParent (gameObject.GetComponent<RectTransform> ());
-		button.transform.SetParent (transform);
+		animator = GetComponent<Animator> ();
 		
 
 	}
@@ -18,6 +15,13 @@ public class test : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
 	{
-	
+		if (Input.GetKey (KeyCode.A)) {
+			animator.CrossFade ("normal", 0.1f);
+			animator.speed = 1;
+		} else if (Input.GetKey (KeyCode.D)) {
+			animator.speed = 1;
+		} else {
+			animator.speed = 0;
+		}
 	}
 }
